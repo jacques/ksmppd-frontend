b8611fe9d7e08036bf3560c0f2ba5797ec8ddf4b
Jacques Marneweck <jacques@powertrip.co.za>
2016-11-06 15:57:42 +0200

    Prep for dev

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Ksmppd Frontend
+ *
+ * @author    Jacques Marneweck <jmarneweck@gmail.com>
+ * @copyright 2016 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+require_once __DIR__ . '/../src/bootstrap.php';
